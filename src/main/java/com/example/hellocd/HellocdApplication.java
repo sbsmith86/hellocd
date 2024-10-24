ddae2c9b76a4169f80f3b9c490b0f4fd97ca5eb5
sbsmith86 <sbsmith86@gmail.com>
2024-10-24 17:45:55 +0000

    updated message

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello to continous delivery!";
 	}
 
 	public static void main(String[] args) {
